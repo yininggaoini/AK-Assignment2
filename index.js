5a3c3ea91dd42eb2d6a1a705b58a663b71fe4840
yininggaoini <sa19b015@technikum-wien.at>
2021-10-13 14:25:38 +0200

    example UI from October 2021, without correct position from data-week.

--- index.js
+++ index.js
@@ -7,10 +7,11 @@ var aktuelWochentag = datum.getDay();
 var kalenderJahr = 2021;
 
 var yearDays = [31,28,31,30,31,30,31,31,30,31,30,31];
+var wochenTag = ["Mon","Dien","Mitt","Donn","Frei","Sam","Sonn"];
 
 var oAktuleDatum = document.getElementById("actual-year-month");
-var oMontasTagen = document.getElementById("monats-tagen");
-oAktuleDatum.innerText = aktuelJahr + ' ' + aktuelMonat; 
+var oMontasTagen = document.getElementById("wochentag");
+oAktuleDatum.innerText = aktuelJahr + '-' + (aktuelMonat+1) + '-' + aktuelTag + ' ' +wochenTag[aktuelWochentag-1]; 
 
 
     var firstDatum = new Date(kalenderJahr,aktuelMonat,1);
@@ -18,13 +19,44 @@ oAktuleDatum.innerText = aktuelJahr + ' ' + aktuelMonat;
     document.write(firstTag);
 
 
-var oTdDay = document.createElement("td");
-
-/* five rows. */
-for(var i=0;i<5;i++){
-
-    /* 7 cols */
-    for(var j=0;j<7;j++){
+    
+   /* for(var i=0;i<yearDays[9];i++){
         
+
+            var oTdDay = document.createElement("td");
+    
+            var oTxt = document.createTextNode(i+1);
+    
+            oTdDay.appendChild(oTxt);
+            oMontasTagen.appendChild(oTdDay);
+         
+        
+}*/
+var daysNumber = 0;
+
+var oBreak = document.createTextNode("\r");
+
+
+
+
+    for(var row=0;row<5;row++){
+
+        var oTdDay = document.createElement("tr");
+        for(var cols=0;cols<7;cols++){
+            
+            if(daysNumber<yearDays[9]){
+
+                var oGezi = document.createElement("td");
+                var oTxt = document.createTextNode(daysNumber+1);
+        
+                oGezi.appendChild(oTxt);
+                oTdDay.appendChild(oGezi);
+                oMontasTagen.appendChild(oTdDay);
+                daysNumber++;
+           
+            }
+        }
+        
+       
     }
-}
+
